b95da3ea9770d6d783d31e8236de3613f01bcab7
Naimur Rahaman <2015naim@gmail.com>
2021-01-24 11:23:50 +0600

    Object Declaration

---
+++ 04.basicobject.js
@@ -0,0 +1,25 @@
+
+// Object declare
+var mehnaz={
+    age:2,
+    weight:12,
+    height: 65,
+    color: "red"
+
+}
+
+var herAge = mehnaz.age;
+console.log(herAge);
+
+var herWeight = mehnaz["weight"];
+console.log(herWeight);
+
+var herHeight = 'height';
+var mehnazHeight = mehnaz[herHeight];
+console.log(mehnazHeight);
+
+
+// update any value
+
+mehnaz.age=2.2;
+console.log(mehnaz);
