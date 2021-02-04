4224119ff5ff9ec110a138f790e1178cadee5812
Naimur Rahaman <2015naim@gmail.com>
2021-02-04 12:24:32 +0600

    filter and find function works

---
+++ 08.filter_find_function.js
@@ -0,0 +1,9 @@
+// const oldArray = [2,5,3,96,7,1];
+// const newArray = oldArray.filter(x => x > 5);
+// console.log(newArray);
+
+// find()
+
+const oldArray = [2,5,3,7,96,1,8];
+const isThere = oldArray.find(x => x > 5);
+console.log(isThere);
