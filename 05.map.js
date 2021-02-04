03655d8140dade8da7c5222d3977eaff98cce2da
Naimur Rahaman <2015naim@gmail.com>
2021-02-04 09:38:37 +0600

    Arrow Function and Map Function Together

--- 05.map.js
+++ 05.map.js
@@ -1,3 +1,12 @@
+// const mapedArray = [11,2,3,5,9];
+
+// const newArray = mapedArray.map(function(element, index, array){
+//     console.log(element, index, array);
+// })
+
+
+
+
 // const mapArray = [2,5,9,8,4,3];
 // const newArray = [];
 // for(let i=0; i<mapArray.length; i++){
@@ -28,10 +37,36 @@
 
 
 
+
+// const mapArray = [2,5,9,8,4,3];
+
+// const newArray = mapArray.map(function(value){
+//     return value + 2;
+// })
+
+// console.log(newArray);
+
+
+
+
+// const mapArray = [2,5,9,8,4,3];
+// function addTwo(value){
+    //     return value + 2;
+    // }
+
+// const newArray = mapArray.map(addTwo);
+// console.log(newArray);
+
+
+
+// const mapArray = [2,5,9,8,4,3];
+// const square = mapArray.map(element => element * element);
+
+// console.log(square);
+
+
+
 const mapArray = [2,5,9,8,4,3];
+const remainder = mapArray.map(x => x % 2);
 
-const newArray = mapArray.map(function(value){
-    return value + 2;
-})
-
-console.log(newArray);
+console.log(remainder);
