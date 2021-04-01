56cf5550138ec6b03c9f7ae718802f027cd9cca0
Naimur Rahaman <2015naim@gmail.com>
2021-04-01 07:49:25 +0600

    25-6.js added

---
+++ 25-6.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(firstName, lastName, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+
+const heroPerson = new Person('Mehnaz', 'Shahrima', 35000);
+console.log(heroPerson);
+
+const specialPerson = new Person('Naimur', 'Rahaman', 50000);
+console.log(specialPerson);
