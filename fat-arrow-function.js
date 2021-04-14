3c05c2cbfd5b85d3bc84a81f09146d69bac558a4
Naimur Rahaman <2015naim@gmail.com>
2021-04-14 22:15:49 +0600

    forEach using arrow function

--- fat-arrow-function.js
+++ fat-arrow-function.js
@@ -19,16 +19,39 @@ const result = sum(5,8);
 console.log(result);
 
 
-// forEach
+// // forEach using normal function
+// const javascript = {
+//     name:"JavaScript",
+//     libraries:["React","Angular","Vue","jQuery"],
+//     library: function() {
+//         // console.log(this);
+        
+//         this.libraries.forEach(function(item){
+//             // console.log(this);
+//             console.log(`${this.name} loves ${item}`);
+//         });
+//     }
+// }
+
+// javascript.library();
+
+
+
+
+// forEach using arrow function
 const javascript = {
     name:"JavaScript",
     libraries:["React","Angular","Vue","jQuery"],
     library: function() {
-        this.libraries.forEach(function(item){
+        // console.log(this);
+        
+        this.libraries.forEach((item) => {
+            // console.log(this);
             console.log(`${this.name} loves ${item}`);
         });
     }
 }
- 
+
 javascript.library();
+
 
