74c758172ab3d4137c1f70df42f9596de6f4bd84
Naimur Rahaman <2015naim@gmail.com>
2021-04-14 21:15:45 +0600

    Fat arrow function

---
+++ fat-arrow-function.js
@@ -0,0 +1,22 @@
+// Part One
+
+const printName = function(name) {
+    console.log(name);
+}
+
+printName('Mehnaz');
+
+
+
+// Arrow function
+const printName1 = name1 => console.log(name1);
+printName1('Shahrima');
+
+
+// Arrow function with one line mathematic instruction
+const sum = (x,y) => x+y;
+const result = sum(5,8);
+console.log(result);
+
+
+
