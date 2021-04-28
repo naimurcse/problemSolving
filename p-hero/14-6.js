77bb9778fb5bde3619983b8f3b8b3e834b9f4fe0
Naimur Rahaman <2015naim@gmail.com>
2021-04-28 06:30:55 +0600

    14-6.js Done Factorial using while update

--- p-hero/14-6.js
+++ p-hero/14-6.js
@@ -1,11 +1,11 @@
 function factorial(num){
     let i = 1;
     let result = 1;
-    while(i<num){
+    while(i<=num){
         result *= i;
         i++;
     }
     console.log(result);
 }
 
-factorial(6);
+factorial(6);  // 6! = 6*5*4*3*2*1 = 720
