bb44b8001816c1c67c0004fa0209a0b6f8581318
Naimur Rahaman <2015naim@gmail.com>
2021-05-03 17:10:31 +0600

    DOM Sumit Project HTML CSS Done

--- DOM/HTML collections/script.js
+++ DOM/HTML collections/script.js
@@ -1,21 +1,29 @@
-let collection = document.getElementsByClassName("item");
-// console.log(collection);
+// let collection = document.getElementsByClassName("item");
+// // console.log(collection);
 
 
-// using for/of iteration
-for (let item of collection) {
-    console.log(item);
-}
+// // using for/of iteration
+// for (let item of collection) {
+//     console.log(item);
+// }
 
 
 
-console.log("-------------------------");
+// console.log("-------------------------");
 
-// Array.from() method converts to an array.
-// console.log(Array.from(collection));
+// // Array.from() method converts to an array.
+// // console.log(Array.from(collection));
 
-let newArr = Array.from(collection);
-newArr.forEach(item => {
-    console.log(item);
-});
+// let newArr = Array.from(collection);
+// newArr.forEach(item => {
+//     console.log(item);
+// });
+
+
+// Select element and assign them to variables
+
+let newTask = document.querySelector('#new_task');
+let form = document.querySelector('form');
+let todoUI = document.querySelector('#items');
+let compateUI = document.querySelector('.complate-list ul');
 
